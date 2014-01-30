b1b808006672d5c78d4b91ba37c960ab341356ba
Minchan Kim <minchan@kernel.org>
2014-01-30 15:46:01 -0800

    aries | p1: zram: remove unnecessary free
    Commit a0c516cbfc74 ("zram: don't grab mutex in zram_slot_free_noity")
    introduced pending zram slot free in zram's write path in case of
    missing slot free by memory allocation failure in zram_slot_free_notify
    but it is not necessary because we have already freed the slot right
    before overwriting.
    
    Signed-off-by: Minchan Kim <minchan@kernel.org>
    Cc: Nitin Gupta <ngupta@vflare.org>
    Cc: Jerome Marchand <jmarchan@redhat.com>
    Tested-by: Sergey Senozhatsky <sergey.senozhatsky@gmail.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    Signed-off-by: Humberto Borba <humberos@gmail.com>
    
    Change-Id: I445a7eec12228831479ba3e47b9bf1b0647e016d

--- drivers/block/zram/zram_drv.c
+++ drivers/block/zram/zram_drv.c
@@ -498,14 +498,6 @@ static int zram_bvec_write(struct zram *zram, struct bio_vec *bvec, u32 index,
 		goto out;
 	}
 
-	/*
-	 * zram_slot_free_notify could miss free so that let's
-	 * double check.
-	 */
-	if (unlikely(meta->table[index].handle ||
-			zram_test_flag(meta, index, ZRAM_ZERO)))
-		zram_free_page(zram, index);
-
 	ret = COMPRESS(uncmem, PAGE_SIZE, src, &clen,
 			       meta->compress_workmem);
 
