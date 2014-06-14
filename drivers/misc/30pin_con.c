4ae3c677555fb221786d2202f24988b80bd50825
Humberto Borba <humberos@gmail.com>
2014-06-14 02:49:08 -0300

    p1: driver: misc: 30pin: handling audio usb
    enable/disable audio usb accordingly with 30pin connector
    
    Change-Id: I41436afdda2f687ad24354653585f31d86ae92b6

--- drivers/misc/30pin_con.c
+++ drivers/misc/30pin_con.c
@@ -173,8 +173,6 @@ static int connector_detect_change(void)
 static void _detected(struct acc_con_info *acc, int device, bool connected)
 {
 
-	enable_audio_usb = false;
-
 	if (connected) {
 		switch(device) {
 #if defined(CONFIG_USB_S3C_OTG_HOST) || defined(CONFIG_USB_DWC_OTG)
@@ -217,9 +215,10 @@ static void _detected(struct acc_con_info *acc, int device, bool connected)
 			break;
 #endif
 		case P30_EARJACK_WITH_DOCK:
-			//to do
+			enable_audio_usb = false;
 			break;
 		case P30_ANAL_TV_OUT:
+			enable_audio_usb = false;
 			TVout_LDO_ctrl(false);
 			break;
 #ifdef CONFIG_KEYBOARD_P1
@@ -495,6 +494,7 @@ static int acc_con_probe(struct platform_device *pdev)
 {
 	struct acc_con_info *acc;
 	int retval = 0;
+
 	pr_info("[30pin] %s\n", __func__);
 
 	acc = kzalloc(sizeof(*acc), GFP_KERNEL);
@@ -503,6 +503,7 @@ static int acc_con_probe(struct platform_device *pdev)
 
 	acc->current_dock = DOCK_NONE;
 	acc->current_accessory = ACCESSORY_NONE;
+	enable_audio_usb = false;
 	dev_set_drvdata(&pdev->dev, acc);
 	acc->acc_dev = &pdev->dev;
 
